b32907d8a49cf36dfc59e73d0efd989ae146f75f
farukakpinar52 <faruk.akpinar@hotmail.com>
2024-06-23 01:25:53 +0300

    Add project files.

---
+++ implicit_explicit_operator_overloading/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+A instance1 = new B();
+A instance2 = (A)new B();
+
+B instance3 = (B)new A();
+
+public class A
+{
+    //B'nin bir instance'ını A'ya kapalı dönüşüm yaptır. Kapalı dönüşüm yapabildiği için açık dönüşümü de yapabilir.
+    public static implicit operator A(B instance)
+    {
+        return new A();
+    };
+}
+public class B
+{
+    //A'nın bir instance'ını B'ye açık dönüşüm yaptır. Açık dönüşüm yapılması belirtildiyse kapalı dönüşümü ayrıca belirtmek gerekecektir.
+    public static explicit operator B(A instance)
+    {
+        return new B();
+    };
+}
